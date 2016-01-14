75e1b2c8843db0b84b21b28a1a9b31ae561cc4c4
Marco Paga <marco.paga@codecentric.de>
2016-01-14 14:44:09 +0100

    Messaging added

--- src/main/scala/de/codecentric/reactiveflows/reactiveflows/FlowFacade.scala
+++ src/main/scala/de/codecentric/reactiveflows/reactiveflows/FlowFacade.scala
@@ -16,16 +16,70 @@
 
 package de.codecentric.reactiveflows.reactiveflows
 
+import java.net.URLEncoder
+
 import akka.actor.{ Props, Actor, ActorLogging }
+import de.codecentric.reactiveflows.reactiveflows.FlowFacade._
 
 object FlowFacade {
   final val Name = "flow-facade";
 
   def props: Props = Props(new FlowFacade)
+
+  case object GetFlows
+
+  case class FlowDescriptor(name: String, label: String)
+
+  case class AddFlow(label: String)
+
+  case class FlowAdded(flowDescriptor: FlowDescriptor)
+
+  case class FlowExists(label: String)
+
+  case class RemoveFlow(name: String)
+
+  case class FlowRemoved(name: String)
+
+  case class FlowUnknown(name: String)
+
 }
 
 class FlowFacade extends Actor with ActorLogging {
 
-  override def receive: Receive = Actor.emptyBehavior
+  import FlowFacade._
 
+  var nameToFlow: Map[String, FlowDescriptor] = Map.empty
+
+  def createName(label: String) = {
+    URLEncoder.encode(label.toLowerCase(), "UTF-8")
+  }
+
+  override def receive: Receive = {
+    case GetFlows => sender() ! nameToFlow.valuesIterator.to[Set]
+
+    case AddFlow(label) if !nameToFlow.contains(createName(label)) => {
+      addFlow(label)
+    }
+    case AddFlow(label) => {
+      sender() ! FlowExists(label)
+    }
+    case RemoveFlow(name) if nameToFlow.contains(name) => {
+      removeFlow(name)
+    }
+    case RemoveFlow(name) => {
+      sender() ! FlowUnknown(name)
+    }
+
+  }
+
+  def removeFlow(name: String): Unit = {
+    nameToFlow -= name
+    sender() ! FlowRemoved(name)
+  }
+
+  def addFlow(label: String): Unit = {
+    val myFlowDescriptor = FlowDescriptor(createName(label), label)
+    nameToFlow += createName(label) -> myFlowDescriptor
+    sender() ! FlowAdded(myFlowDescriptor)
+  }
 }
